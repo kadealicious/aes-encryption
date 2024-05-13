adcbf1f552297b7d593e16640d4df54bbf37a8be
kadealicious <kadesamson@gmail.com>
2024-05-12 20:03:50 -0700

    Add DecryptBlock()

--- aes.cpp
+++ aes.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 #include<string>
+#include<fstream>
+#include<sstream>
+
+using namespace std;
+
+typedef unsigned char BYTE;
 
 /// Display the menu and get user input.
 char DisplayMenu()
@@ -13,17 +19,63 @@ char DisplayMenu()
 	return getchar();
 }
 
+/// Return data from a file given a path.
 string ReadFile(string file_path)
 {
-	
+	ifstream file_stream(file_path);
+	string file_data;
+	if(file_stream)
+	{
+		ostringstream ss;
+		ss << file_stream.rdbuf();
+		file_data = ss.str();
+		return file_data;
+	}
+	cout << "File \"" << file_path << "\" not found!" << endl;
+	return "";
 }
 
-void EncryptFile(char key[16], string file_data) {
-	
+/// Encrypt a file using AES-128.
+void EncryptFile(BYTE key[16], string unencrypted_file_path)
+{
+	string file_data = ReadFile(unencrypted_file_path);
+	if(file_data == "")
+	{
+		cout << "Nothing to encrypt!" << endl;
+		return;
+	}
+	cout << "Unencrypted file data: " << file_data << endl;
 }
 
-void DecryptFile(char key[16], string file_data) {
+/** Decrypt a block a AES-128 cipher text; if successful, will place a cstring 
+	into the decrypted_block parameter. */
+void DecryptBlock(BYTE input_block[16], BYTE (&decrypted_block)[16])
+{
+	// AES-128 always has 10 rounds of encryption and an exp. key size of 176.
+	const int round_count		= 10;
+	const int expanded_key_size	= 176;
+	BYTE expanded_key[expanded_key_size];
+	
+	// Swap rows and columns (AES is column major).
+	for(int i = 0; i < 4; i++)
+	{
+		for(int j = 0; j < 4; j++)
+		{
+			decrypted_block[(i + (j * 4))] = input_block[(i * 4) + j];
+		}
+	}
+}
 
+/// Decrypt a file that was encrypted using AES-128.
+void DecryptFile(BYTE key[16], string encrypted_file_path)
+{
+	string file_data = ReadFile(encrypted_file_path);
+	if(file_data == "")
+	{
+		cout << "Nothing to decrypt!" << endl;
+		return;
+	}
+	cout << "Encrypted file data: " << file_data << endl;
 }
 
 int main(int argc, char* argv[])
@@ -31,8 +83,7 @@ int main(int argc, char* argv[])
 	printf("AES Encryption Demo - Kade Samson & Ari Salehpour\n");
 	
 	char user_choice	= '\0';
-	string key			= "thisisthekey!1!!";
-	string file_data	= NULL;
+	BYTE key[16]		= {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'};
 	
 	// While we have not quit, cycle through user input and options.
 	while(user_choice != 'Q' && user_choice != 'q')
@@ -53,7 +104,7 @@ int main(int argc, char* argv[])
 			// Decrypt file.
 			case 'D':
 			case 'd':
-				DecryptFile(key, "secret_message.txt");
+				DecryptFile(key, "secret_message_encrypted.txt");
 				break;
 
 			default:
