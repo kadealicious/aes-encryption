e424ea65c986b5a694e6deb101289cc8cdb550f5
kadealicious <kadesamson@gmail.com>
2024-05-12 18:04:56 -0700

    Structure

--- aes.cpp
+++ aes.cpp
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include<iostream>
+#include<string>
 
 /// Display the menu and get user input.
 char DisplayMenu()
 {
 	printf("Please choose a command:\n");
-	printf(" - [R]ead a file\n");
 	printf(" - [E]ncrypt\n");
 	printf(" - [D]ecrypt\n");
 	printf(" - [Q]uit the program\n");
@@ -13,43 +13,54 @@ char DisplayMenu()
 	return getchar();
 }
 
+string ReadFile(string file_path)
+{
+	
+}
+
+void EncryptFile(char key[16], string file_data) {
+	
+}
+
+void DecryptFile(char key[16], string file_data) {
+
+}
+
 int main(int argc, char* argv[])
 {
 	printf("AES Encryption Demo - Kade Samson & Ari Salehpour\n");
 	
+	char user_choice	= '\0';
+	string key			= "thisisthekey!1!!";
+	string file_data	= NULL;
+	
 	// While we have not quit, cycle through user input and options.
-	char user_choice = '\0';
 	while(user_choice != 'Q' && user_choice != 'q')
 	{
 		// After the user enters input, don't display the menu twice.
-		if(user_choice != '\n') {
-			user_choice = DisplayMenu();
-		} else {
-			user_choice = getchar();
-		}
+		if(user_choice != '\n')	{ user_choice = DisplayMenu(); }
+		else					{ user_choice = getchar(); }
 
 		// Handle user choices.
 		switch(user_choice)
 		{
-			// Read file.
-			case 'R':
-			case 'r':
-				break;
-			
 			// Encrypt file.
 			case 'E':
 			case 'e':
+				EncryptFile(key, "secret_message.txt");
 				break;
 
 			// Decrypt file.
 			case 'D':
 			case 'd':
+				DecryptFile(key, "secret_message.txt");
 				break;
 
 			default:
 				break;
 		}
 	}
-
+	
+	printf("Bye bye!\n");
 	return 0;
 }
